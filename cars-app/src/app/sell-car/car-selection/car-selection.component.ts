18181d8efddf333a8a6d166b79c8dfc9dec03725
rakeshbaddam1008 <rakeshbaddam1008@gmail.com>
2023-07-13 22:00:06 -0400

    changes

--- cars-app/src/app/sell-car/car-selection/car-selection.component.ts
+++ cars-app/src/app/sell-car/car-selection/car-selection.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-car-selection',
   templateUrl: './car-selection.component.html',
-  styleUrls: ['./car-selection.component.css']
+  styleUrls: ['./car-selection.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class CarSelectionComponent {
-
-}
+export class CarSelectionComponent {}
