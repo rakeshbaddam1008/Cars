317a88f7d284b62621c83d812cecc2ffaa0ccd45
MohammedMukthar <mdmukthar007yu@email.com>
2023-07-17 21:52:51 +0530

    made changes to stepper con color

--- cars-app/src/app/sell-car/car-stepper/car-stepper.component.ts
+++ cars-app/src/app/sell-car/car-stepper/car-stepper.component.ts
@@ -3,11 +3,13 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ISellerVechileDetails } from 'src/app/models/ISellerVechileDetails';
 import { IVechileData, IVechileModelDetails } from 'src/app/models/IVechile';
 import { SellCarStoreService } from 'src/app/services/SellCarStore.Service';
+import { ViewEncapsulation, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-car-stepper',
   templateUrl: './car-stepper.component.html',
   styleUrls: ['./car-stepper.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CarStepperComponent {
   secondFormGroup = this._formBuilder.group({
@@ -18,9 +20,31 @@ export class CarStepperComponent {
 
   constructor(
     private _formBuilder: FormBuilder,
-    public _store: SellCarStoreService
+    public _store: SellCarStoreService,
+    private renderer: Renderer2
   ) {
     // this._store.loadSellerDetails();
     this.selectVechileDetails = this._store.sellerCompleteDetails.vechile;
+  }
+
+  ngAfterViewInit() {
+    const stepperHeaderElements = document.querySelectorAll('.mat-step-icon');
+    
+    stepperHeaderElements.forEach((headerElement, index) => {
+      const color = this.getStepIconColor(index);
+      this.renderer.setStyle(headerElement, 'background-color', color);
+    });
+  }
+  getStepIconColor(stepIndex: number): string {
+    switch (stepIndex) {
+      case 0:
+        return '#ff0000'; // Red
+      case 1:
+        return '#ffd800'; // yellow
+      case 2:
+        return '#ffd800'; // green
+      default:
+        return '#10ff0a'; 
+    }
   }
 }
