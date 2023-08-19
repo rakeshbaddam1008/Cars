7bbb11314081deb304536a8871595e7521416016
rakeshbaddam1008 <rakeshbaddam1008@gmail.com>
2023-08-18 21:55:02 -0400

    changes

--- cars-app/src/app/services/AuthService.ts
+++ cars-app/src/app/services/AuthService.ts
@@ -19,7 +19,7 @@ export class AuthService {
     return this.http.post(
       AUTH_API + '/login',
       {
-        username,
+        email_id: username,
         password,
       },
       httpOptions
