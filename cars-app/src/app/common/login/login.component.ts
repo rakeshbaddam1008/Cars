47aa25ff0e0dcef4f709c4ac37636a74209f1998
rakeshbaddam1008 <rakeshbaddam1008@gmail.com>
2023-08-18 21:53:00 -0400

    changes

--- cars-app/src/app/common/login/login.component.ts
+++ cars-app/src/app/common/login/login.component.ts
@@ -1,71 +1,147 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
+import { AuthService } from 'src/app/services/AuthService';
+import { TokenStorageService } from 'src/app/services/TokenStorageService';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  loginFormGroup: FormGroup = new FormGroup({})
+  //   form: any = {
+  //   username: null,
+  //   password: null
+  // };
+
+  loginFormGroup: FormGroup = new FormGroup({});
   logintemplate: boolean = true;
-  signupFormGroup: FormGroup = new FormGroup({})
-  signuptemplate:boolean = false;
-  passwordFormGroup: FormGroup = new FormGroup({})
+  signupFormGroup: FormGroup = new FormGroup({});
+  signuptemplate: boolean = false;
+  passwordFormGroup: FormGroup = new FormGroup({});
   passwordtemplate: boolean = false;
-  constructor(private router: Router, public toaster: ToastrService,) {
+  isLoggedIn: boolean = false;
+  isLoginFailed: boolean = false;
+  errorMessage: any;
+  constructor(
+    private router: Router,
+    public toaster: ToastrService,
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) {
     this.loginFormGroup = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.required, Validators.email]
-      ),
-      password: new FormControl('', Validators.required)
-    })
-    this.signupFormGroup = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.required, Validators.email]
-      ),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
-      confirmPassword: new FormControl('', Validators.required)
-    })
+    });
+    this.signupFormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    });
     this.passwordFormGroup = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.required, Validators.email]
-      )
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
   }
 
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  signup() {
+    let username = this.signupFormGroup.controls['email'].value;
+    let pwd = this.signupFormGroup.controls['password'].value;
+    let confirmPassword =
+      this.signupFormGroup.controls['confirmPassword'].value;
+    if (pwd == confirmPassword) {
+      alert('Password mismatch');
+    }
+
+    this.authService.register(username, pwd).subscribe(
+      (data) => {
+        console.log(data);
+        this.toaster.warning(
+          'Sign up succesfull, proceed to login.',
+          'Success',
+          {
+            timeOut: 4000,
+            positionClass: 'toast-top-right',
+            closeButton: true,
+          }
+        );
+        this.logintemplate = true;
+        this.signuptemplate = false;
+        this.passwordtemplate = false;
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+        this.toaster.warning(
+          'Error occured during sign-up, please try later.',
+          'Warning',
+          {
+            timeOut: 4000,
+            positionClass: 'toast-top-right',
+            closeButton: true,
+          }
+        );
+      }
+    );
+  }
 
   routeToDashboard() {
     if (this.loginFormGroup.valid) {
-      this.router.navigateByUrl('/dashboard')
+      let username = this.loginFormGroup.controls['email'].value;
+      let pwd = this.loginFormGroup.controls['password'].value;
+      this.authService.login(username, pwd).subscribe(
+        (data) => {
+          this.tokenStorage.saveToken(data.accessToken);
+          this.tokenStorage.saveRefreshToken(data.refreshToken);
+          this.tokenStorage.saveUser(data);
+
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+          this.router.navigateByUrl('/dashboard');
+        },
+        (err) => {
+          this.errorMessage = err.error.message;
+          this.isLoginFailed = true;
+          this.toaster.warning('Error occured during Login', 'Warning', {
+            timeOut: 4000,
+            positionClass: 'toast-top-right',
+            closeButton: true,
+          });
+        }
+      );
     } else {
-      this.toaster.warning('Please fill all the required Fields', 'Warning', { timeOut: 4000, positionClass: 'toast-top-right', closeButton: true })
-
+      this.toaster.warning('Please fill all the required Fields', 'Warning', {
+        timeOut: 4000,
+        positionClass: 'toast-top-right',
+        closeButton: true,
+      });
     }
-
   }
 
   toggleBetweenLoginPages(event: string) {
-    if(event === 'login') {
+    if (event === 'login') {
       this.logintemplate = true;
       this.signuptemplate = false;
       this.passwordtemplate = false;
-    } else if(event === 'signUp') {
+    } else if (event === 'signUp') {
       this.logintemplate = false;
       this.signuptemplate = true;
       this.passwordtemplate = false;
-    }
-    else if(event === 'forgetPassword') {
+    } else if (event === 'forgetPassword') {
       this.logintemplate = false;
       this.signuptemplate = false;
       this.passwordtemplate = true;
     }
   }
-
 }
