21e3aad3fcdd0ea0ff0ee58463f2f453520fa8f3
rakeshbaddam1008 <rakeshbaddam1008@gmail.com>
2023-08-19 13:35:11 -0400

    changes

--- cars-app/src/app/navigation/header/header.component.ts
+++ cars-app/src/app/navigation/header/header.component.ts
@@ -12,28 +12,7 @@ export class HeaderComponent {
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit(): void {
-    var data = '';
-
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener('readystatechange', function () {
-      if (this.readyState === 4) {
-        console.log(this.responseText);
-      }
-    });
-
-    xhr.open('GET', 'http://localhost:8080/carizma/seller-vehicle-info');
-    xhr.setRequestHeader(
-      'Authorization',
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVSUB3aWxsY294c3lzdGVtcy5jb20iLCJpYXQiOjE2OTI0NjA2NTYsImV4cCI6MTY5MjQ2MTU1Nn0.1I-sj7M73WDLTK4rpLDwLzDtz5k38phVJQphWjPL8-0'
-    );
-
-    var response = xhr.send(data);
-
-    alert(response);
-  }
+  ngOnInit(): void {}
 
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
